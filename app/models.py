9b041da5705f08fd35f93e5159b56acd5534adbd
Gaurav <gaurav.ssnl@gmail.com>
2021-12-12 16:52:01 +0530

    Add User DB Model.

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+from . import db
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    
